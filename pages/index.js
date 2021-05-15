8cd3883024b6eece36226765ecf001b61313b343
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-05-15 20:39:31 +0700

    update index url

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        <h1 style={{color:'red'}}>Index page</h1>
-        <p className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi qui, pariatur quia facere consequatur maxime dignissimos, sapiente ratione velit eum temporibus omnis, commodi placeat! Expedita cumque laudantium magnam facilis velit.</p>
-        <img src="/images/1.jpg" alt="" widht="200"/>
-
+      <a href="/index2">index2</a>
+      <a href="/backend/dashboard">/backend/dashboard</a>
+      <h1 style={{ color: "red" }}>Index page</h1>
+      <p className="desc">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi qui,
+        pariatur quia facere consequatur maxime dignissimos, sapiente ratione
+        velit eum temporibus omnis, commodi placeat! Expedita cumque laudantium
+        magnam facilis velit.
+      </p>
+      <img src="/images/1.jpg" alt="" widht="200" />
     </div>
-  )
+  );
 }
